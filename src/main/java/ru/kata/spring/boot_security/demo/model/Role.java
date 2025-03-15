d6141d88a8f77795a8fe5a5b87b340b25a3e4784
Rudakov Ivan <ivan1792@gmail.com>
2025-03-15 16:08:47 +0500

    Сделал классы + таблицы

--- src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
@@ -3,21 +3,59 @@ package ru.kata.spring.boot_security.demo.model;
 import org.springframework.security.core.GrantedAuthority;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "roles")
-public class Role implements GrantedAuthority {
+public class Role {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @Column(unique = true, nullable = false)
     private String name;
+    @ManyToMany(mappedBy = "roles")
+    private Set <User> users = new HashSet<>();
 
-    @Override
-    public String getAuthority() {
-        return "";
+
+    public Role() {
+    }
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public void addUserToRole(User user) {
+        this.users.add(user);
+    }
+
+    public void removeUserToRole(User user) {
+        this.users.remove(user);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
     }
 }
 
--- src/main/java/ru/kata/spring/boot_security/demo/model/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/User.java
@@ -1,69 +1,77 @@
 package ru.kata.spring.boot_security.demo.model;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
 import javax.persistence.*;
-import java.util.Collection;
-import java.util.List;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 
 @Entity
 @Table(name = "users")
-public class User implements UserDetails{
+public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private int id;
+    private Long id;
+    @Column(unique = true)
+    private String username;
     @Column
-    private String login;
-    @Column
-    private String pass;
+    private String password;
     @Column
     private String name;
     @Column
     private String surname;
     @Column
     private int age;
-    @ManyToMany(fetch = FetchType.EAGER)
-    private Set<Role> role;
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "users_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
 
-    public User(String login, String pass, String name, String surname, int age, Set<Role> role) {
-        this.login = login;
-        this.pass = pass;
-        this.name = name;
-        this.surname = surname;
-        this.age = age;
-        this.role = role;
-    }
+    )
+    private Set<Role> roles = new HashSet<>();
 
     public User() {
     }
 
-    public int getId() {
+    public User(String username, String password, String name, String surname, int age) {
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public void addRoleToUser(Role role) {
+        this.roles.add(role);
+    }
+
+    public void removeRoleToUser(Role role) {
+        this.roles.remove(role);
+    }
+
+
+    public Long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
-    public String getLogin() {
-        return login;
+    public String getUsername() {
+        return username;
     }
 
-    public void setLogin(String login) {
-        this.login = login;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
-    public String getPass() {
-        return pass;
+    public String getPassword() {
+        return password;
     }
 
-    public void setPass(String password) {
-        this.pass = password;
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public String getName() {
@@ -90,56 +98,12 @@ public class User implements UserDetails{
         this.age = age;
     }
 
-    @Override
-    public String toString() {
-        return "id:" + id + " " + surname + " " + name + " " + age + " age.";
+    public Set<Role> getRoles() {
+        return roles;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return id == user.id && age == user.age && Objects.equals(name, user.name) && Objects.equals(surname, user.surname);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, surname, age);
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
-    }
-
-    @Override
-    public String getPassword() {
-        return getPass();
-    }
-
-    @Override
-    public String getUsername() {
-        return getLogin();
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
+    public void setRoles(Set<Role> role) {
+        this.roles = role;
     }
 }
+
